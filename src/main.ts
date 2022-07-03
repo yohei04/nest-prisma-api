1fccb8fe2612c4478c92a8a6069c64460267fea2
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-07-03 13:45:09 +0900

    chore: install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js example')
+    .setDescription('Nest.js API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
